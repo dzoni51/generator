55f5a93fcd377c7e4fddf5c4ce653a77c3d21779
johnny-celavi <kovacevicnikola51@gmail.com>
2022-11-03 16:43:29 +0100

    wip

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,6 @@ import Config
 # which you should run after static files are built and
 # before starting your production server.
 config :generator, GeneratorWeb.Endpoint,
-  url: [host: "139.162.139.178", port: 4000],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
